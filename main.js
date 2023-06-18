c916cb0014a613fe514646860dcbd9369fd3c255
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-06-18 18:40:17 +0200

    function to fetch data from api

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-const url = 'https://moviesdatabase.p.rapidapi.com/titles';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '35e2e8e49fmsh8ec08757228c66fp1a054bjsna3d86f42c8fb',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
-};
-
-async function fetchData() {
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-};
-
-console.log(fetchData());
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2VjYzJmNTAyNTUwNWYzYTYyMzc3OGZhNzgzMTY0NiIsInN1YiI6IjYzZTdjNmQ2MTI3Nzc4MDA4OTVmMDg3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wtHvOjgCOPgJELSD4gF3ofi_H71MW1P5QxxaCHCMHCQ'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
