195ad1254b497e93180aedeae404a238a9ed38a2
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-06-27 23:03:37 +0200

    extend function to display img and title for the movies in the api response

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 
 
+
 async function getMovies() {
-  let response = await fetch ('https://api.themoviedb.org/3/trending/all/day?language=en-US'); 
+  let response = await fetch('https://api.themoviedb.org/3/trending/all/week?language=en-US&api_key=ccecc2f5025505f3a623778fa7831646');
   let data = await response.json();
   return data;
 }
 
-getMovies().then(response=> {
+
+getMovies().then(response => {
+  const apiResponse = document.querySelector(".apiResponse");
   console.log(response);
-  const apiResponse = document.getElementsByClassName("apiResponse")[0];
-  apiResponse.textContent = JSON.stringify (response);
-})
+
+  response.results.forEach(movie => {
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie");
+
+    const imageUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+    const title = movie.original_title || movie.original_name;
+
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    image.alt = "Movie Poster";
+
+    const heading = document.createElement("h3");
+    heading.textContent = title;
+
+    movieContainer.appendChild(image);
+    movieContainer.appendChild(heading);
+
+    apiResponse.appendChild(movieContainer);
+  });
+  
+});
 
 
 
 
-
-   
