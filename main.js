1055456822b5259195dadbc1397a775e5ee768df
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-06-27 22:06:57 +0200

    refactor the api request function

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2VjYzJmNTAyNTUwNWYzYTYyMzc3OGZhNzgzMTY0NiIsInN1YiI6IjYzZTdjNmQ2MTI3Nzc4MDA4OTVmMDg3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wtHvOjgCOPgJELSD4gF3ofi_H71MW1P5QxxaCHCMHCQ'
-    }
-  };
-  
-  fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
-    .then(response => response.json())
-    // .then(response => console.log(response))
-    .then(data => {
-      const apiResponse = document.getElementsByClassName("apiResponse")[0];
-      apiResponse.textContent = JSON.stringify(data);
-      
-    })
-    .catch(err => console.error(err));
+
+
+async function getMovies() {
+  let response = await fetch ('https://api.themoviedb.org/3/trending/all/day?language=en-US'); 
+  let data = await response.json();
+  return data;
+}
+
+getMovies().then(response=> {
+  console.log(response);
+  const apiResponse = document.getElementsByClassName("apiResponse")[0];
+  apiResponse.textContent = JSON.stringify (response);
+})
+
+
+
+
+
    
